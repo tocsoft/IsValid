4c51d9d8a8bd4328e05bbfbf10c78ee0418d6fcd
Scott Williams <scott.williams@2020legal.co.uk>
2015-10-23 11:28:36 +0100

    is numeric

---
+++ IsValid.Tests/String/IsNumeric.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using IsValid;
+using System.Threading;
+using System.Globalization;
+
+namespace IsValid.Tests.String
+{
+    [TestFixture]
+    public class IsNumeric
+    {
+
+        [Test]
+        [TestCase("123", ExpectedResult = true)]
+        [TestCase("Foo", ExpectedResult = false)]
+        [TestCase("123Foo123", ExpectedResult = false)]
+        public bool IsNumericTest(string value)
+        {
+            return value.IsValid().Numeric();
+        }
+    }
+}
---
+++ IsValid/String/IsNumeric.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace IsValid
+{
+    public static class IsNumeric
+    {
+        /// <summary>
+        /// Indicates whether input is in correct format for a number and is valid.
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static bool Numeric(this IValidatableValue<string> inputValue)
+        {
+            var input = inputValue.Value;
+            if (input == null)
+            {
+                return false;
+            }
+            int length = input.Length;
+            if (length == 0)
+            {
+                return false;
+            }
+            int i = 0;
+            if (input[0] == '-')
+            {
+                if (length == 1)
+                {
+                    return false;
+                }
+                i = 1;
+            }
+            for (; i < length; i++)
+            {
+                char c = input[i];
+                if (c <= '/' || c >= ':')
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+    }
+}
