a5bd9ed875e5131408569ceaf5372e458dad38c1
Scott Williams <scott.williams@2020legal.co.uk>
2015-10-23 11:29:14 +0100

    Is IPAddress

---
+++ IsValid.Tests/String/IsIPAddress.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using IsValid;
+using System.Threading;
+using System.Globalization;
+using System.Net.Sockets;
+
+namespace IsValid.Tests.String
+{
+    [TestFixture]
+    public class IsIPAddress
+    {
+        public IEnumerable<string> IPv4
+        {
+            get
+            {
+                yield return "127.0.0.1";
+                yield return "0.0.0.0";
+                yield return "255.255.255.255";
+            }
+        }
+
+        public IEnumerable<string> NotIPv4
+        {
+            get
+            {
+                yield return "256.0.0.0";
+                yield return "26.0.0.256";
+                yield return "abc";
+                yield return "::1";
+            }
+        }
+        public IEnumerable<string> IPv6
+        {
+            get
+            {
+                yield return "::1";
+                yield return "2001:db8:0000:1:1:1:1:1";
+            }
+        }
+
+        public IEnumerable<string> NotIPv6
+        {
+            get
+            {
+                yield return "127.0.0.1";
+                yield return "0.0.0.0";
+            }
+        }
+
+        public IEnumerable<string> AnyIP
+        {
+            get
+            {
+                return IPv6.Union(IPv4);
+            }
+        }
+        public IEnumerable<string> NotAnyIP
+        {
+            get
+            {
+                var allNots = NotIPv6.Union(NotIPv4);
+                return allNots.Where(x => !AnyIP.Contains(x));
+            }
+        }
+
+
+
+        [Test]
+        [TestCaseSource("IPv4")]
+        public void IsIPv4(string value)
+        {
+            Assert.IsTrue(value.IsValid().IPAddressV4());
+        }
+
+        [Test]
+        [TestCaseSource("IPv4")]
+        public void IsIPv4ViaFamily(string value)
+        {
+            Assert.IsTrue(value.IsValid().IPAddress(AddressFamily.InterNetwork));
+        }
+
+        [Test]
+        [TestCaseSource("NotIPv4")]
+        [TestCaseSource("IPv6")]
+        public void IsNotIPv4(string value)
+        {
+            Assert.IsFalse(value.IsValid().IPAddressV4());
+        }
+
+        [Test]
+        [TestCaseSource("NotIPv4")]
+        [TestCaseSource("IPv6")]
+        public void IsNotIPv4ViaFamily(string value)
+        {
+            Assert.IsFalse(value.IsValid().IPAddress(AddressFamily.InterNetwork));
+        }
+
+        [Test]
+        [TestCaseSource("IPv6")]
+        public void IsIPv6(string value)
+        {
+            Assert.IsTrue(value.IsValid().IPAddressV6());
+        }
+        [Test]
+        [TestCaseSource("IPv6")]
+        public void IsIPv6ViaFamily(string value)
+        {
+            Assert.IsTrue(value.IsValid().IPAddress(AddressFamily.InterNetworkV6));
+        }
+
+        [Test]
+        [TestCaseSource("NotIPv6")]
+        [TestCaseSource("IPv4")]
+        public void IsNotIPv6(string value)
+        {
+            Assert.IsFalse(value.IsValid().IPAddressV6());
+        }
+
+        [Test]
+        [TestCaseSource("NotIPv6")]
+        [TestCaseSource("IPv4")]
+        public void IsNotIPv6ViaFamily(string value)
+        {
+            Assert.IsFalse(value.IsValid().IPAddress(AddressFamily.InterNetworkV6));
+        }
+
+        [Test]
+        [TestCaseSource("AnyIP")]
+        public void IsAnyIP(string value)
+        {
+            Assert.IsTrue(value.IsValid().IPAddress());
+        }
+
+        [Test]
+        [TestCaseSource("NotAnyIP")]
+        public void IsNotAnyIP(string value)
+        {
+            Assert.IsFalse(value.IsValid().IPAddress());
+        }
+    }
+}
---
+++ IsValid/String/IsIPAddress.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace IsValid
+{
+    public static class IsIPAddress
+    {
+        /// <summary>
+        /// Determine whether input is a valid IPv4 address 
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static bool IPAddressV4(this IValidatableValue<string> input)
+        {
+            return input.IPAddress(System.Net.Sockets.AddressFamily.InterNetwork);
+        }
+
+        /// <summary>
+        /// Determine whether input is a valid IPv6 address 
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static bool IPAddressV6(this IValidatableValue<string> input)
+        {
+            return input.IPAddress(System.Net.Sockets.AddressFamily.InterNetworkV6);
+        }
+
+        /// <summary>
+        /// Determine whether input is a valid IP address of the address families
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static bool IPAddress(this IValidatableValue<string> input, params System.Net.Sockets.AddressFamily[] family)
+        {
+
+            if (family == null || !family.Any())
+            {
+                return false;
+            }
+
+            IPAddress address;
+            if (System.Net.IPAddress.TryParse(input.Value, out address))
+            {
+                return family.Contains(address.AddressFamily);
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Determine whether input is a valid IP address
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static bool IPAddress(this IValidatableValue<string> input)
+        {
+            return input.IPAddress(System.Net.Sockets.AddressFamily.InterNetwork, System.Net.Sockets.AddressFamily.InterNetworkV6);
+        }
+    }
+}
