6fea73db28cb3c9782bc548400dbb7648aaf7183
Scott Williams <scott.williams@2020legal.co.uk>
2015-10-23 11:30:09 +0100

    is creditcard

---
+++ IsValid.Tests/String/IsCreditCard.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using IsValid;
+using System.Threading;
+using System.Globalization;
+
+namespace IsValid.Tests.String
+{
+    [TestFixture]
+    public class IsCreditCard
+    {
+
+        [Test]
+        [TestCase("375556917985515", ExpectedResult = true)]
+        [TestCase("36050234196908", ExpectedResult = true)]
+        [TestCase("4716461583322103", ExpectedResult = true)]
+        [TestCase("4716-2210-5188-5662", ExpectedResult = true)]
+        [TestCase("4929 7226 5379 7141", ExpectedResult = true)]
+        [TestCase("5398228707871527", ExpectedResult = true)]
+        [TestCase("Foo", ExpectedResult = false)]
+        [TestCase("Bar123", ExpectedResult = false)]
+        [TestCase("5398228707871528", ExpectedResult = false)]
+
+        public bool TestCreditCard(string value)
+        {
+            return value.IsValid().CreditCard();
+        }
+    }
+}
---
+++ IsValid/String/IsCreditCard.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace IsValid
+{
+    public static class IsCreditCard
+    {
+        /// <summary>
+        /// Indicates whether input is in correct format for a credit card number and is valid.
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static bool CreditCard(this IValidatableValue<string> inputV)
+        {
+
+            var input = inputV.Value.Replace(" ", "");
+            input = input.Replace("-", "");
+            if (!input.IsValid().Numeric())
+            {
+                return false;
+            }
+            int sumOfDigits = input
+                .Where((e) => e >= '0' && e <= '9')
+                .Reverse()
+                .Select((e, i) => ((int)e - 48) * (i % 2 == 0 ? 1 : 2))
+                .Sum((e) => e / 10 + e % 10);
+            return sumOfDigits % 10 == 0;
+        }
+
+    }
+}
