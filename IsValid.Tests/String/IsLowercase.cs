56406ca0d5ff107799a848254fcf9261df818a0e
Scott Williams <scott.williams@2020legal.co.uk>
2015-10-23 11:28:08 +0100

    is lowercase

---
+++ IsValid.Tests/String/IsLowercase.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using IsValid;
+using System.Threading;
+using System.Globalization;
+
+namespace IsValid.Tests.String
+{
+    [TestFixture]
+    public class IsLowercase
+    {
+
+        [Test]
+        [TestCase("foo", ExpectedResult = true)]
+        [TestCase("FOO", ExpectedResult = false)]
+        [TestCase("123", ExpectedResult = true)]
+        [TestCase("foo123", ExpectedResult = true)]
+        [TestCase("FOO123", ExpectedResult = false)]
+        [TestCase("Foo123", ExpectedResult = false)]
+        public bool IsLowercaseTest(string value)
+        {
+            return value.IsValid().Lowercase();
+        }
+    }
+}
---
+++ IsValid/String/IsLowercase.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace IsValid
+{
+    public static class IsAllLowercase
+    {
+        /// <summary>
+        /// Determine whether input is in lower case.
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static bool Lowercase(this IValidatableValue<string> input)
+        {
+            if(!input.IsValueSet && input.Value == null)
+            {
+                return true;
+            }
+
+            var val = input.Value;
+            return val == val.ToLower();
+        }
+    }
+}
