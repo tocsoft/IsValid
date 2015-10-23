68f20b0136e1a957f8c0d38be46bd2bbeb8be24e
Scott Williams <scott.williams@2020legal.co.uk>
2015-10-23 11:27:25 +0100

    is uppercase

---
+++ IsValid.Tests/String/IsUppercase.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using IsValid;
+using System.Threading;
+using System.Globalization;
+
+namespace IsValid.Tests.String
+{
+    [TestFixture]
+    public class IsUppercase
+    {
+
+        [Test]
+        [TestCase("foo", ExpectedResult = false)]
+        [TestCase("FOO", ExpectedResult = true)]
+        [TestCase("123", ExpectedResult = true)]
+        [TestCase("foo123", ExpectedResult = false)]
+        [TestCase("FOO123", ExpectedResult = true)]
+        [TestCase("Foo123", ExpectedResult = false)]
+        public bool IsUppercaseTest(string value)
+        {
+            return value.IsValid().Uppercase();
+        }
+    }
+}
---
+++ IsValid/String/IsUppercase.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace IsValid
+{
+    public static class IsUppercase
+    {
+        /// <summary>
+        /// Determine whether input is in lower case.
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static bool Uppercase(this IValidatableValue<string> input)
+        {
+            if (!input.IsValueSet && input.Value == null)
+            {
+                return true;
+            }
+
+            var val = input.Value;
+            return val == val.ToUpper();
+        }
+    }
+}
