5ec9348e3bddaf6b954edcdf9a21ca83499f9a15
Scott Williams <scott.williams@2020legal.co.uk>
2015-10-23 11:29:40 +0100

    is email address

---
+++ IsValid.Tests/String/IsEmail.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using IsValid;
+using System.Threading;
+using System.Globalization;
+
+namespace IsValid.Tests.String
+{
+    [TestFixture]
+    public class IsEmail
+    {
+
+        [Test]
+        [TestCase("foo@bar.com", ExpectedResult = true)]
+        [TestCase("foo@bar.com.au", ExpectedResult = true)]
+        [TestCase("foo+bar@bar.com", ExpectedResult = true)]
+        [TestCase("invalidemail@", ExpectedResult = false)]
+        [TestCase("invalid.com", ExpectedResult = false)]
+        [TestCase("@invalid.com", ExpectedResult = false)]
+        public bool TestEmail(string value)
+        {
+            return value.IsValid().Email();
+        }
+    }
+}
---
+++ IsValid/String/IsEmail.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace IsValid
+{
+    public static class IsEmail
+    {
+        /// <summary>
+        /// Determine whether input matches a valid email address.
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static bool Email(this IValidatableValue<string> input)
+        {
+            try
+            {
+                return new MailAddress(input.Value).Address == input.Value;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+    }
+}
