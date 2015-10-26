9599d896c20dcc1cebe3dce4ae6605dc601f08d7
Scott Williams <scott.williams@2020legal.co.uk>
2015-10-26 10:22:12 +0000

    added messages to help debug current stage

--- build.cmd
+++ build.cmd
@@ -1,4 +1,6 @@
 @echo Off
+ECHO Starting build
+
 set config=%1
 if "%config%" == "" (
    set config=Release
@@ -13,26 +15,32 @@ if "%nuget%" == "" (
   set nuget=nuget.exe
 )
 
+ECHO Running MSBUILD
 REM Build
 %WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild IsValid.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
 
 if not "%GallioEcho%" == "" (  
+
+  ECHO Running unit tests
   REM Unit tests
   %GallioEcho% IsValid\IsValid.Tests\bin\%config%\IsValid.Tests.dll
   if not "%errorlevel%"=="0" goto failure
 )
 
 REM Package
+ECHO Building pacakges
 mkdir Build
 call %nuget% pack "IsValid\IsValid.csproj" -IncludeReferencedProjects -o Build -p Configuration=%config% %version%
 if not "%errorlevel%"=="0" goto failure
 
 
 :success
+ECHO successfully built project
 exit 0
 goto end
 
 :failure
+ECHO failed to build.
 exit -1
 goto end
 
