e46de839728bd717e99b6f94706b07a7c2d5dac9
Scott Williams <scott.williams@2020legal.co.uk>
2015-12-14 13:00:37 +0000

    support different exit codes

--- IsValid/String/IsMobilePhone.cs
+++ IsValid/String/IsMobilePhone.cs
@@ -12,24 +12,24 @@ namespace IsValid
     {
         private static Dictionary<string, Regex> LocaleMobilePhoneRegexes = new Dictionary<string, Regex>
         {
-            { "zh-CN", new Regex(@"^((\+|00)?0?86\-?)?1[345789][0-9]{9}$", RegexOptions.Compiled) },
-            { "zh-TW", new Regex(@"^((\+|00)?886\-?|0)?9\d{8}$", RegexOptions.Compiled) },
-            { "en-ZA", new Regex(@"^((\+|00)?27|0)(\d{9})$", RegexOptions.Compiled) },
-            { "en-AU", new Regex(@"^((\+|00)?61|0)4(\d{8})$", RegexOptions.Compiled) },
-            { "fr-FR", new Regex(@"^((\+|00)?33|0)(6|7)\d{8}$", RegexOptions.Compiled) },
-            { "en-HK", new Regex(@"^((\+|00)?852\-?)?[569]\d{3}\-?\d{4}$", RegexOptions.Compiled) },
-            { "pt-PT", new Regex(@"^((\+|00)351)?9[1236]\d{7}$", RegexOptions.Compiled) },
-            { "el-GR", new Regex(@"^((\+|00)30)?((2\d{9})|(69\d{8}))$", RegexOptions.Compiled) },
-            { "en-GB", new Regex(@"^((\+|00)?44|0)7\d{9}$", RegexOptions.Compiled) },
-            { "en-US", new Regex(@"^((\+|00)?1)?[2-9]\d{2}[2-9](?!11)\d{6}$", RegexOptions.Compiled) },
-            { "en-ZM", new Regex(@"^((\+|00)26)?09[567]\d{7}$", RegexOptions.Compiled) },
-            { "ru-RU", new Regex(@"^((\+|00)?7|8)?9\d{9}$", RegexOptions.Compiled) },
-            { "nb-NO", new Regex(@"^((\+|00)?47)?[49]\d{7}$", RegexOptions.Compiled) },
-            { "nn-NO", new Regex(@"^((\+|00)?47)?[49]\d{7}$", RegexOptions.Compiled) },
-            { "nl-NL", new Regex(@"^((\+|00)31|0)6\d{7}$", RegexOptions.Compiled) }
+            { "zh-CN", new Regex(@"^(\+?0?86\-?)?1[345789][0-9]{9}$", RegexOptions.Compiled) },
+            { "zh-TW", new Regex(@"^(\+?886\-?|0)?9\d{8}$", RegexOptions.Compiled) },
+            { "en-ZA", new Regex(@"^(\+?27|0)(\d{9})$", RegexOptions.Compiled) },
+            { "en-AU", new Regex(@"^(\+?61|0)4(\d{8})$", RegexOptions.Compiled) },
+            { "fr-FR", new Regex(@"^(\+?33|0)(6|7)\d{8}$", RegexOptions.Compiled) },
+            { "en-HK", new Regex(@"^(\+?852\-?)?[569]\d{3}\-?\d{4}$", RegexOptions.Compiled) },
+            { "pt-PT", new Regex(@"^(\+351)?9[1236]\d{7}$", RegexOptions.Compiled) },
+            { "el-GR", new Regex(@"^(\+30)?((2\d{9})|(69\d{8}))$", RegexOptions.Compiled) },
+            { "en-GB", new Regex(@"^(\+?44|0)7\d{9}$", RegexOptions.Compiled) },
+            { "en-US", new Regex(@"^(\+?1)?[2-9]\d{2}[2-9](?!11)\d{6}$", RegexOptions.Compiled) },
+            { "en-ZM", new Regex(@"^(\+26)?09[567]\d{7}$", RegexOptions.Compiled) },
+            { "ru-RU", new Regex(@"^(\+?7|8)?9\d{9}$", RegexOptions.Compiled) },
+            { "nb-NO", new Regex(@"^(\+?47)?[49]\d{7}$", RegexOptions.Compiled) },
+            { "nn-NO", new Regex(@"^(\+?47)?[49]\d{7}$", RegexOptions.Compiled) },
+            { "nl-NL", new Regex(@"^(\+31|0)6\d{7}$", RegexOptions.Compiled) }
         };
 
-        private static bool IsLocalPhone(ValidatableValue<string> phoneNumber, string locale)
+        private static bool IsLocalPhone(ValidatableValue<string> phoneNumber, string locale, string exitCode)
         {
             if (!phoneNumber.IsValueSet || string.IsNullOrWhiteSpace(phoneNumber.Value))
             {
@@ -39,7 +39,14 @@ namespace IsValid
             Regex localeRegex;
             if (LocaleMobilePhoneRegexes.TryGetValue(locale, out localeRegex))
             {
-                return localeRegex.IsMatch(phoneNumber.Value);
+                var number = phoneNumber.Value;
+
+                if (exitCode != null && number.StartsWith(exitCode, StringComparison.OrdinalIgnoreCase))
+                {
+                    number = "+" + number.Substring(exitCode.Length);
+                }
+
+                return localeRegex.IsMatch(number);
             }
             return false;
         }
@@ -54,7 +61,23 @@ namespace IsValid
         /// </remarks>
         public static bool MobilePhone(this ValidatableValue<string> phoneNumber)
         {
-            if (!phoneNumber.Locale.Any(loc => IsLocalPhone(phoneNumber, loc)))
+            return MobilePhone(phoneNumber, null);
+        }
+
+        /// <summary>
+        /// Determines whether the given phone number is a mobile phone number or not. Based on the current locale of the executing thread
+        /// </summary>
+        /// <param name="phoneNumber">The phone number to check.</param>
+        /// <param name="exitCode">The exit code.</param>
+        /// <returns>
+        /// True if it is a mobile phone number, false otherwise.
+        /// </returns>
+        /// <remarks>
+        /// Relies on locales that use specific blocks of numbers for mobile phone numbers.
+        /// </remarks>
+        public static bool MobilePhone(this ValidatableValue<string> phoneNumber, string exitCode)
+        {
+            if (!phoneNumber.Locale.Any(loc => IsLocalPhone(phoneNumber, loc, exitCode)))
             {
                 phoneNumber.AddError("Not a recognised mobile phone number");
             }
